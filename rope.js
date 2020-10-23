2bb5a85f73eff5f0aa60a3892671ce4cb56ed0ca
ismaeelbiyabani <72075716+ismaeelbiyabani@users.noreply.github.com>
2020-10-23 11:09:14 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,36 @@
+class Rope 
+{
+    constructor(body1,body2,offsetX,offsetY){
+        this.offsetX=offsetX
+        this.offsetY=offsetY
+
+        var options={
+            bodyA:body1,
+            bodyB:body2,
+            pointB:{x:this.offsetX,y:this.offsetY}
+        }
+        this.rope=Constraint.create(options)
+        World.add(world,this.rope)
+       
+    
+    }
+display(){
+    rope1=new Rope(bob1.body,groundObject.body,-bob*2,0)
+    rope2=new Rope (bob2.body,groundObject.body,-bob*2,0)
+    rope3=new Rope (bob3.body,groundObject.body,-bob*2,0)
+    
+
+    var pointA=this.rope.bodyA.position;
+    var pointB=this.rope.bodyB.position;
+
+    strokeWeight(4);
+
+    var Anchor1X=pointA.x
+    var Anchor1X=poinyB.y
+
+    var Anchor2X=pointB.x+this.offsetX
+    var Anchor2Y=pointB.y+this.offsetY
+
+    line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y);
+}
+}
